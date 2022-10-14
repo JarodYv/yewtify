bca48fa48bb9b4ef30308de33fe4f76225744bbb
Jarodyv <jarodyv@gmail.com>
2022-10-14 17:04:28 +0800

    back to `sass_rs`

--- build.rs
+++ build.rs
@@ -1,21 +1,18 @@
 use anyhow::{anyhow, Error};
-use rsass::{compile_scss_path, output};
+use sass_rs::{compile_file, Options, OutputStyle};
 use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
 
 fn main() -> Result<(), Error> {
-    let path = "./styles/yewtify.sass".as_ref();
-    let format = output::Format {
-        style: output::Style::Compressed,
-        ..Default::default()
-    };
-    let css = compile_scss_path(path, format)
+    let mut opts = Options::default();
+    opts.output_style = OutputStyle::Compact;
+    let data = compile_file("./styles/yewtify.sass", opts)
         .map_err(|err| anyhow!("sass builder failed with: {}", err))?;
     let mut path = PathBuf::from(env::var("YEWTIFY_OUT")?);
     path.push("yewtify.css");
     let mut file = File::create(path)?;
-    file.write_all(&css)?;
+    file.write_all(data.as_bytes())?;
     Ok(())
 }
